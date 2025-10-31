8da4eea5d7a5ea3b7cccc0bd77aa27d81224c898
victormori <victordaab@gmail.com>
2025-10-31 20:59:42 -0300

    adiciona id e email ao customer

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -3,12 +3,14 @@ import { UpsertDTO } from "./dto/upsert.dto";
 
 @Injectable()
 export class CustomersService {
-   private customers: Array<object>;
+   private customers: Array<any>;
    // método especial - ele é chamado na criação
    constructor() {
      this.customers = [
         {
+            "id": 1,
             "nome": "Félix",
+            "email": "felix@gmail.com",
             "idade": 18
         }
      ]
@@ -19,7 +21,14 @@ export class CustomersService {
    }
 
    create(customer: UpsertDTO) {
-     this.customers.push(customer);
+     let id = 1;
+     if(this.customers.length != 0) {
+        id = this.customers[this.customers.length - 1].id + 1
+     }
+     this.customers.push({
+      "id": id,
+      ...customer
+     });
 
      return {
         "message": "Salvo com sucesso"
--- src/customers/dto/upsert.dto.ts
+++ src/customers/dto/upsert.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class UpsertDTO {
     @IsNotEmpty()
     nome: string;
     
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
     @IsNotEmpty()
     idade: number;
 }
